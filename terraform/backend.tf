538114fadf9aefc2bd75b8b69bcac980409ab352
Abhishek Panda <abhishek.panda@nettantra.net>
2025-06-07 18:07:03 +0530

    Refactor backend.tf to use variable for S3 bucket and add state_bucket variable definition

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "infra-terraform-state-07"
+    bucket         = var.state_bucket
     key            = "tiny/dev.tfstate"
     region         = "us-east-1"
     use_lockfile = true
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,9 @@ variable "key_name" {
   description = "my-key-pair"
   type        = string
 }
+
+variable "state_bucket" {
+  description = "S3 bucket for Terraform state"
+  type        = string
+  
+}
